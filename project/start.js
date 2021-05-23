ad255a8abf8eb3797b78eeb055cf18a9e0466d55
Deepam <Deepamaggarwal068@gmail.com>
2021-05-23 17:51:13 +0530

    added pages

--- project/start.js
+++ project/start.js
@@ -13,6 +13,14 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/about', function (request, response) {
+    response.render('pages/about');
+});
+
+app.get('/project', function (request, response) {
+    response.render('pages/project');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
