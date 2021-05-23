b4ff4e5fa1cf372e62183bf35c403eb9ad310a9a
Deepam <Deepamaggarwal068@gmail.com>
2021-05-23 22:04:47 +0530

    temperature

--- project/js/main.js
+++ project/js/main.js
@@ -9,3 +9,14 @@ function myFunction() {
   $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
 }
 
+$(document).ready(function () { getWeather(); })
+
+function getWeather() {
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=Boston&units=imperial&appid=" + apiKey;
+    $.ajax(url, {
+        success: function (data) {
+            console.log(data);
+            $(".city").text(data.name);
+            $(".temp").text(data.main.temp);
+        }})
+}
