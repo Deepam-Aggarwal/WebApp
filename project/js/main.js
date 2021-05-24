c37262cffdd9f3e417a4d5c29009baedc7850246
Deepam <Deepamaggarwal068@gmail.com>
2021-05-24 20:54:03 +0530

    Temperature API

--- project/js/main.js
+++ project/js/main.js
@@ -5,13 +5,19 @@ $(document).ready(function(){
 
 function getWeather(searchQuery) {
     var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=imperial&appid=" + apiKey;
+
+    $(".error-message").text("");
+    $(".city").text("");
+    $(".temp").text();
+
     $.ajax(url, {
         success: function (data) {
-            console.log(data);
             $(".city").text(data.name);
             $(".temp").text(data.main.temp);
-        }
-    })
+        }, error: function (error) {
+            $(".error-message").text("An error occured");
+            }
+        })
 }
 
 function searchWeather() {
