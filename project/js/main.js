8545603881ab689684e7133822d9d8bb1bbf39a1
Deepam <Deepamaggarwal068@gmail.com>
2021-05-26 14:31:11 +0530

    form

--- project/js/main.js
+++ project/js/main.js
@@ -51,12 +51,12 @@ function handlesignin() {
 }
 
 
-function addMessage(name1, email1, subject1, message1) {
+function addMessage(namee, emaill, subjectt, messagee) {
     var postData = {
-        Name: name1,
-        Email: email1,
-        Subject: subject1,
-        Message: message1
+        nameofuser: namee,
+        emailofuser: emaill,
+        subjectofuser: subjectt,
+        messageofuser: messagee
     }
     var database = firebase.database().ref("posts");
    
@@ -72,9 +72,9 @@ function addMessage(name1, email1, subject1, message1) {
 }
 
 function handleMessage() {
-    var name1 = $("#form_name").val();
-    var email1 = $("#form_email").val();
-    var subject1 = $("#form_subject").val();
-    var message1 = $("#form_message").val();
-    addMessage(name1, email1, subject1, message1);
+    var namee = $("#form_name").val();
+    var emaill = $("#form_email").val();
+    var subjectt = $("#form_subject").val();
+    var messagee = $("#form_message").val();
+    addMessage(namee, emaill, subjectt, messagee);
 }
